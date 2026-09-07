f3e4f8a7424e72ffbb9eec24ffe28f900b3edc2e
NethumiJ <jknethu@gmail.com>
2026-09-07 11:45:01 +0530

    Completed Nethumi_34954

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Home(),
-    ),
-  );
+  runApp(const MaterialApp(debugShowCheckedModeBanner: false, home: Home()));
 }
 
 class Home extends StatelessWidget {
@@ -25,24 +20,33 @@ class Home extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text("Network Image:", style: TextStyle(fontWeight: FontWeight.bold)),
+            const Text(
+              "Network Image:",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
             const SizedBox(height: 10),
-            
-            // TODO 1: Display an image from the internet.
-            // - Replace the SizedBox below with an Image.network() widget.
-            // - Use this URL: "https://plus.unsplash.com/premium_photo-1683910767532-3a25b821f7ae?w=400"
-            const SizedBox(height: 150, width: 150),
-
+            Image.network(
+              "https://plus.unsplash.com/premium_photo-1683910767532-3a25b821f7ae?w=400",
+              height: 150,
+              width: 150,
+              fit: BoxFit.cover,
+            ),
             const SizedBox(height: 40),
-
-            const Text("Local Asset Image:", style: TextStyle(fontWeight: FontWeight.bold)),
+            const Text(
+              "Local Asset Image:",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
             const SizedBox(height: 10),
-
             // TODO 3: Display a local image from your project folder.
             // - Ensure you have completed TODO 2 in pubspec.yaml first!
             // - Replace the SizedBox below with an Image.asset() widget.
             // - Use the path: "assets/img-2.webp" (make sure this file exists in your repository).
-            const SizedBox(height: 150, width: 150),
+            Image.asset(
+              "assets/img-2.webp",
+              height: 150,
+              width: 150,
+              fit: BoxFit.cover,
+            ),
           ],
         ),
       ),
